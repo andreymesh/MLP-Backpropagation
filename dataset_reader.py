e7a56c92e7910e1b6e732c2b9c7b237ec7d84e33
Lennoximus <lennoximus@gmail.com>
2017-12-11 20:17:31 +0300

    Add proper MLPClassifier solution

--- dataset_reader.py
+++ dataset_reader.py
@@ -3,15 +3,53 @@ import pandas
 
 
 def get_worksheet(file_path):
-  raw_book = xlrd.open_workbook(file_path,
-                                formatting_info=True)
-  return raw_book.sheet_by_index(0)
+    raw_book = xlrd.open_workbook(file_path,
+                                  formatting_info=True)
+    return raw_book.sheet_by_index(0)
+
+
+# мужской - 1
+# женский - 0
+# да - 1
+# нет - 0
+# возможный - 0.5
+# очень высокий - 1
+# высокий - 0.75
+# умеренный - 0.5
+# низкий - 0.25
+# очень низкий - 0
+# курит - 1
+# не курит - 0
 
 
 def get_dataset(file_path):
-  worksheet = get_worksheet(file_path)
-  dataset = []
-  for row_index in range(1, worksheet.nrows):
-    dataset.append([rowElement for rowElement in worksheet.row_values(row_index, 1) if rowElement])
+    worksheet = get_worksheet(file_path)
+    dataset = []
+    for row_index in range(1, worksheet.nrows):
+        row = []
+        for row_element in worksheet.row_values(row_index, 1):
+            if row_element != '':
+                if (row_element == 'мужской') | (row_element == 'да') | (row_element == 'очень высокий') | (
+                        row_element == 'курит') | (row_element == '1,00') | (row_element == '1.00') | (
+                        row_element == 1):
+                    row.append(float(1))
+                elif row_element == 'высокий':
+                    row.append(float(0.75))
+                elif (row_element == 'умеренный') | (row_element == 'возможный'):
+                    row.append(float(0.5))
+                elif row_element == 'низкий':
+                    row.append(float(0.25))
+                elif (row_element == 'женский') | (row_element == 'нет') | (row_element == 'очень низкий') | (
+                        row_element == 'не курит') | (row_element == '0,00') | (row_element == '0.00') | (
+                        row_element == 0):
+                    row.append(float(0))
+                else:
+                    row.append(row_element)
 
-  return pandas.DataFrame(dataset)
+                # print(row)
+
+        dataset.append(row)
+
+    # dataset.append([rowElement for if rowElement])
+
+    return pandas.DataFrame(dataset)
--- index.py
+++ index.py
@@ -1,38 +1,35 @@
 import dataset_reader
-import encoding
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 excel_workbook_path = 'D:\PycharmProjects\PythonLabs\Нейронные сети\BackPropagation\\athero.xls'
 
 input_dataset = dataset_reader.get_dataset(excel_workbook_path).values
 
-x_vector = input_dataset[:, 1:24]
-encoded_x_vector = encoding.x_encoding(x_vector)
+x_vector = input_dataset[:, 1:23]
 y_vector = input_dataset[:, 0]
-encoded_y_vector = encoding.y_encoding(y_vector)
 
-# newDataset = []
+x_train, x_test, y_train, y_test = train_test_split(x_vector, y_vector, test_size=0.30)
 
-# for datasetRow in x_vector:
-#     print(datasetRow)
+mlp_classifier = MLPClassifier(hidden_layer_sizes=(6, 1),
+                               max_iter=100,
+                               alpha=1e-5,
+                               solver='lbfgs',
+                               activation='logistic',
+                               learning_rate_init=.5)
 
-# print(x_vector[0])
-# lc.fit(x_vector[0])
+mlp_classifier.fit(x_train, y_train)
 
-# for row in x_vector:
-#     print(lc.fit(row))
+print("Training set accuracy: %f" % mlp_classifier.score(x_train, y_train))
+print("Test set accuracy: %f" % mlp_classifier.score(x_test, y_test))
 
-
-
-# lc.fit(x_vector)
-
-# X = lc.transform(x_vector)
-# Y = lc.transform(y_vector)
-# print(X)
-# print(Y)
-# print(lc.fit(inputDataset)[0][0])
-# print(scaler.fit(inputDataset[0]))
-
-# clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-# clf.fit(inputDataset, [0, 1])
+seed = 2605
+estimators = []
+estimators.append(('mlp', mlp_classifier))
+pipeline = Pipeline(estimators)
+kfold = KFold(shuffle=True, random_state=seed)
+results = cross_val_score(pipeline, x_vector, y_vector, cv=kfold, pre_dispatch='5')
+print("Result: %.2f%% +-(%.2f%%)" % (results.mean() * 100, results.std() * 100))
